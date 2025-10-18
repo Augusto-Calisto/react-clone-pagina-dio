827944264a10f91bed66b960f6ce5bef77b366f5
Augusto Calisto <augustoaquino007@gmail.com>
2025-10-18 18:34:50 -0300

    Criacao componente CreateAccount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Feed } from "./pages/feed";
 
 import { Home } from './pages/home'
 import { Login } from './pages/login'
+import { CreateAccount } from "./pages/create-account";
+
 import { GlobalStyle } from './styles/global';
 
 function App() {
-  return (
-    <Router>
-     <GlobalStyle />
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />} />
-     </Routes >
-    </Router>
-  );
+	return (
+		<Router>
+			<GlobalStyle />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/feed" element={<Feed />} />
+				<Route path="/create-account" element={<CreateAccount />} />
+			</Routes >
+		</Router>
+	);
 }
 
 export default App;
